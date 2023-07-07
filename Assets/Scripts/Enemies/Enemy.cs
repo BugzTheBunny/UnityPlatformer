f5bd6b130b0ae16925fdf53a4df09d24bde56d12
unknown <urigami2010@gmail.com>
2023-07-07 08:58:47 +0300

    added ghost enemy

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -37,7 +37,7 @@ public class Enemy : MonoBehaviour
         FreezOnZ();
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController>() != null)
         {
--- Assets/Scripts/Enemies/Ghost/Enemy_Ghost.cs
+++ Assets/Scripts/Enemies/Ghost/Enemy_Ghost.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -8,14 +9,18 @@ public class Enemy_Ghost : Enemy
     [Header("Ghost Specifics")]
     [SerializeField] private float activeTime;
                      private float activeTimeTimer;
-
+    [SerializeField] private float[] xOffset;
 
     private Transform player;
+    private SpriteRenderer sr;
     protected override void Start()
     {
         base.Start();
         player = GameObject.Find("Player").transform;
+        sr = GetComponent<SpriteRenderer>();
         activeTimeTimer = activeTime;
+        aggresive = true;
+        invincible = true;
     }
 
     private void Update()
@@ -41,6 +46,18 @@ public class Enemy_Ghost : Enemy
             activeTimeTimer = activeTime;
 
         }
+        OnXAxisMove();
+    }
+
+    public void Desappear()
+    {
+        sr.enabled = false;
+    }
+
+    public void Appear()
+    {
+        AppearPosition();
+        sr.enabled = true;
     }
 
     public override void Damaged()
@@ -48,6 +65,33 @@ public class Enemy_Ghost : Enemy
         base.Damaged();
     }
 
-    
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (aggresive)
+        {
+            base.OnTriggerEnter2D(collision);
+        }
+    }
 
+    private void AppearPosition()
+    {
+        float yOffset = Random.Range(-7, 7);
+        float xoffsetValue = xOffset[Random.Range(0, xOffset.Length)];
+        transform.position = new Vector2(player.transform.position.x + xoffsetValue, player.transform.position.y + yOffset);
+    }
+
+    protected override void OnXAxisMove()
+    {
+        if (facingDirection == -1 && transform.position.x < player.transform.position.x)
+        {
+            Flip();
+            Debug.Log("Flip!");
+        }
+        else if (facingDirection == 1 && transform.position.x > player.transform.position.x)
+        {
+            Flip();
+            Debug.Log("Flip!2");
+
+        }
+    }
 }
